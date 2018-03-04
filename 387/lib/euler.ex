dbf43715cce308cb93084c035f7f8bb9e062afae
Rik Hemsley <rik@hemsley.cc>
2018-03-04 21:49:51 +0000

    387 passes initial test

--- 387/lib/euler.ex
+++ 387/lib/euler.ex
@@ -1,29 +1,94 @@
 defmodule Euler do
-  def sum_digits(n), do: Enum.sum(Integer.digits(n, 10))
-  def harshad?(n), do: rem(n, sum_digits(n)) == 0
-  def truncate(n) when n >= 10, do: trunc(n/10)
-  def truncate(_), do: nil
-  def recursive_truncate(n) do
-    Stream.unfold(n, fn x ->
-      q = truncate(x)
-      case q do
-        nil -> nil
-        _ -> {q, q}
-      end
-    end
-    )
+  ### Utilities
+
+  @spec denary_digits(integer) :: [integer, ...]
+  def denary_digits(n), do: Integer.digits(n, 10)
+
+  @spec divisible_by?(integer, integer) :: boolean
+  def divisible_by?(numerator, denominator), do: rem(numerator, denominator) == 0
+
+  @spec sum_digits(integer) :: integer
+  def sum_digits(n), do: Enum.sum(denary_digits(n))
+
+  @spec right_truncatable?(integer) :: boolean
+  def right_truncatable?(n), do: n >= 10
+
+  @spec right_truncate(integer) :: integer
+  def right_truncate(n) do
+    unless right_truncatable?(n), do: raise ArgumentError, message: "Not right-truncatable: #{n}"
+    trunc(n / 10)
   end
 
-  def strong?(n), do: Prime.prime?(n/sum_digits(n))
-  def right_truncatable_harshad?(n), do: Enum.all?(recursive_truncate(n), &harshad?/1)
-  def strong_right_truncatable_harshad?(n), do: right_truncatable_harshad?(n) && strong?(n)
+  # @spec recursive_right_truncate(integer) :: [integer, ...]
+  # Dialyzer: "The success typing is (_) -> fun(({'cont',_} | {'halt',_} | {'suspend',_},_)
+  # -> {'done',_} | {'halted',_} | {'suspended',_,fun((_) -> any())})
+  # I don't know if it's possible to determine the actual output type statically.
+  # I think I'll not bother trying to write a spec then!
+  # |> Enum.to_list would give [integer, ...]
+  def recursive_right_truncate(n) do
+    Stream.unfold(n, fn x ->
+      case right_truncatable?(x) do
+        false -> nil
+        true ->
+          q = right_truncate(x) 
+          {q, q}
+      end
+    end)
+  end
 
+  ### Definitions
+
+  # "A Harshad or Niven number is a number that is divisible by the sum of its
+  # digits."
+  @spec harshad?(integer) :: boolean
+  def harshad?(n), do: divisible_by?(n, sum_digits(n))
+
+  # "Let's call a Harshad number that, while recursively truncating the last
+  # digit, always results in a Harshad number a right truncatable Harshad
+  # number."
+  @spec right_truncatable_harshad?(integer) :: boolean
+  def right_truncatable_harshad?(n) do
+    unless harshad?(n), do: raise("Not harshad: #{n}")
+
+    Enum.all?(recursive_right_truncate(n), &harshad?/1)
+  end
+
+  # "Let's call a Harshad number that, when divided by the sum of its digits,
+  # results in a prime a strong Harshad number."
+  @spec strong_harshad?(integer) :: boolean
+  def strong_harshad?(n) do
+    unless harshad?(n), do: raise("Not harshad: #{n}")
+
+    Prime.prime?(div(n, sum_digits(n)))
+  end
+
+  @spec strong_right_truncatable_harshad_number?(integer) :: boolean
+  def strong_right_truncatable_harshad_number?(n) do
+    harshad?(n) && strong_harshad?(n) && right_truncatable_harshad?(n)
+  end
+
+  # "Now take the number 2011 which is prime. When we truncate the last digit
+  # from it we get 201, a strong Harshad number that is also right truncatable.
+  # Let's call such primes strong, right truncatable Harshad primes."
+  # => A strong, right-truncatable Harshad prime is:
+  #  o A prime
+  #  ... which, when right-truncated, results in ...
+  #  o A strong, right-truncatable Harshad number
+  @spec strong_right_truncatable_harshad_prime?(integer) :: boolean
+  def strong_right_truncatable_harshad_prime?(n) do
+    unless Prime.prime?(n), do: raise("Not prime: #{n}")
+    unless right_truncatable?(n), do: raise("Not right-truncatable: #{n}")
+
+    strong_right_truncatable_harshad_number?(right_truncate(n))
+  end
+
+  @spec solve(integer) :: integer
   def solve(upper_bound) do
-    Prime.sequence
-    |> Enum.take_while(fn(n) -> n < upper_bound end)
-    |> Enum.filter(&strong_right_truncatable_harshad?/1)
+    Prime.sequence()
+    |> Enum.take_while(fn n -> n < upper_bound end)
+    |> Enum.filter(&right_truncatable?/1)
+    |> Enum.filter(&strong_right_truncatable_harshad_prime?/1)
     |> IO.inspect
-    |> Enum.sum
+    |> Enum.sum()
   end
 end
-
--- 387/test/euler_test.exs
+++ 387/test/euler_test.exs
@@ -2,44 +2,119 @@ defmodule EulerTest do
   use ExUnit.Case
   doctest Euler
 
-  test "201 is harshad" do
+  test "201 is Harshad" do
     assert Euler.harshad?(201)
   end
 
-  test "201 truncated is 20" do
-    assert 20 == Euler.truncate(201)
+  test "Hardy–Ramanujan number (1729) is a Harshad number" do
+    assert Euler.harshad?(1729)
   end
 
-  test "20 truncated is 2" do
-    assert 2 == Euler.truncate(20)
+  test "Harshads match A005349 in OES" do
+    assert [
+             1,
+             2,
+             3,
+             4,
+             5,
+             6,
+             7,
+             8,
+             9,
+             10,
+             12,
+             18,
+             20,
+             21,
+             24,
+             27,
+             30,
+             36,
+             40,
+             42,
+             45,
+             48,
+             50,
+             54,
+             60,
+             63,
+             70,
+             72,
+             80,
+             81,
+             84,
+             90,
+             100,
+             102,
+             108,
+             110,
+             111,
+             112,
+             114,
+             117,
+             120,
+             126,
+             132,
+             133,
+             135,
+             140,
+             144,
+             150,
+             152,
+             153,
+             156,
+             162,
+             171,
+             180,
+             190,
+             192,
+             195,
+             198,
+             200
+           ] ==
+           Stream.unfold(1, fn(n) -> {n, n+1} end)
+           |> Enum.take_while(fn(n) -> n <= 200 end)
+           |> Enum.filter(&Euler.harshad?/1)
+           |> Enum.to_list
   end
 
-  test "201 is right truncatable harshad number" do
+  test "201 right-truncated is 20" do
+    assert 20 == Euler.right_truncate(201)
+  end
+
+  test "20 right-truncated is 2" do
+    assert 2 == Euler.right_truncate(20)
+  end
+
+  test "2 right-truncated isn't possible" do
+    assert_raise ArgumentError, "Not right-truncatable: 2", fn -> Euler.right_truncate(2) end
+  end
+
+  test "recursive_right_truncate recursively right-truncates" do
+    assert [201, 20, 2] == Euler.recursive_right_truncate(2011) |> Enum.to_list()
+  end
+
+  test "201 is right truncatable Harshad number" do
     assert Euler.right_truncatable_harshad?(201)
   end
 
-  test "2011 is strong harshad number" do
-    assert Euler.strong?(2011)
+  test "201 is strong Harshad number" do
+    assert Euler.strong_harshad?(201)
   end
 
-  test "2011 is right truncatable harshad number" do
-    assert Euler.right_truncatable_harshad?(2011)
-  end
-
-  test "2011 is strong right truncatable harshad prime" do
-    assert Euler.strong_right_truncatable_harshad?(2011)
-  end
-
-  test "truncate" do
-    assert [20, 2] == Euler.recursive_truncate(201) |> Enum.to_list
+  test "2011 is strong right truncatable Harshad prime" do
+    assert Euler.strong_right_truncatable_harshad_prime?(2011)
   end
 
   test "primes" do
-    assert [2, 3, 5, 7, 11] == Prime.sequence() |> Enum.take(5) |> Enum.to_list
+    assert [2, 3, 5, 7, 11] == Prime.sequence() |> Enum.take(5) |> Enum.to_list()
   end
 
   test "one" do
     assert 90619 == Euler.solve(10_000)
   end
 
+  test "10^14" do
+    #assert :mu == Euler.solve(100_000_000_000_000)
+  end
 end
